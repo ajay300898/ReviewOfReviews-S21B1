d3914c6db5749ab04372f853b4527a2381ce0208
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-09-19 21:32:05 +0530

    Update ConsoleWriterTests.cs

--- SenderTests/ConsoleWriterTests.cs
+++ SenderTests/ConsoleWriterTests.cs
@@ -8,8 +8,26 @@ using Xunit;
 
 namespace SenderTests
 {
-    class ConsoleWriterTests
+    public class ConsoleWriterTests
     {
+        public Func<string, string[]> testFunc = line => new string[]{"Passed"};
+        ConsoleWriter writer = new ConsoleWriter();
 
+        [Fact]
+        public void TestGetRowFromLineInvokesPassedFunc()
+        {
+            
+            string[] result = writer.GetRowFromLine("test line", testFunc);
+            string expected = "Passed";
+            Assert.Equal(expected,result[0]);
+        }
+
+        [Fact]
+        public void TestGetSeriesOfWordsInvokesPassedFunc()
+        {
+            string[] result = writer.GetSeriesOfWords("test line", testFunc);
+            string expected = "Passed";
+            Assert.Equal(expected, result[0]);
+        }
     }
 }
