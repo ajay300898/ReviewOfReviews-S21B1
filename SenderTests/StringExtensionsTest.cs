c078c1f51f3193788701d14dd93a3667d8bad977
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-09-19 21:32:50 +0530

    Update StringExtensionsTest.cs

--- SenderTests/StringExtensionsTest.cs
+++ SenderTests/StringExtensionsTest.cs
@@ -8,7 +8,24 @@ using Xunit;
 
 namespace SenderTests
 {
-    class StringExtensionsTest
+    public class StringExtensionsTest
     {
+        [Fact]
+        public void TestStringContainsByIgnoringCase()
+        {
+            string subString = "IamSubString";
+            string mainString = "IambigStringIIncludeiamsubstring";
+            Assert.True(mainString.Contains(subString, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [Fact]
+        public void RaiseExceptionIfSubstringIsNull()
+        {
+            string subString = null;
+            string mainString = "IambigStringIIncludeiamsubstring";
+            Assert.Throws<ArgumentNullException>(() => mainString.Contains(subString, StringComparison.OrdinalIgnoreCase));
+        }
+
+        
     }
 }
