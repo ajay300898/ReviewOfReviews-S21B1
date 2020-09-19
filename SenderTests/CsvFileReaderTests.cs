8a514d807fcbb63f7d3ee96a9da2025532745725
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-09-19 19:49:07 +0530

    Update CsvFileReaderTests.cs

--- SenderTests/CsvFileReaderTests.cs
+++ SenderTests/CsvFileReaderTests.cs
@@ -14,7 +14,7 @@ namespace SenderTests
         [Fact]
         public void TestForFilteringColumnsWhenRequiredColumnsAreGiven()
         {
-            var path = "C:/Users/Ajay kumar/Desktop/Sample3.csv";
+            var path = @"D:\a\ReviewOfReviews-S21B1\ReviewOfReviews-S21B1\Sample3.csv";
             string[] result = reader.Read(path, new string[] { "ReviewDate", "Comments" });
             string[] actualStrings = new string[]{ "ReviewDate,Comments",
                                                     "4/27/2020 9:14,what does this help with?",
